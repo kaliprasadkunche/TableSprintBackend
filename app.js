3c64caaa271d880d0228b44fec2870144d4684c0
kaliprasadkunche <113325469+kaliprasadkunche@users.noreply.github.com>
2024-09-15 21:14:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const createToken = (id) => {
 const createUsersTable = `
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 )
 `;
